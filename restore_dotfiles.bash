1912683ca8be5f4430ad0f76ca951d179952e42a
Ben Holley <benolee@gmail.com>
2011-02-22 21:03:53 -0600

    update vim plugins

---
+++ restore_dotfiles.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -s .bashrc ~/.bashrc;
+ln -s .screenrc ~/.screenrc;
+ln -s .vimrc ~/.vimrc;
+ln -s .vim ~/.vim;
+ln -s .gemrc ~/.gemrc;
+ln -s .gitconfig ~/.gitconfig;
+ln -s .irssi ~/.irssi;
