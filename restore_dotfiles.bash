db119e7ca98599c399a3e629d1030a7646420030
Ben Holley <benolee@gmail.com>
2011-04-26 17:31:40 -0500

    remove restore_dotfiles.bash since it doesn't work

--- restore_dotfiles.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ln -s .bashrc ~/.bashrc;
-ln -s .screenrc ~/.screenrc;
-ln -s .vimrc ~/.vimrc;
-ln -s .vim ~/.vim;
-ln -s .gemrc ~/.gemrc;
-ln -s .gitconfig ~/.gitconfig;
-ln -s .irssi ~/.irssi;
